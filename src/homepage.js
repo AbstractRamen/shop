75b1c35192ae8f74d0de5b28ca1757a918ee13b3
AbstractRamen <jasperctools@gmail.com>
2020-05-21 23:55:04 +0900

    sass

---
+++ src/homepage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            Hat
+          </h1>
+          <span className="subtitle">
+            Shop now
+          </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            Cloak
+          </h1>
+          <span className="subtitle">
+            Shop now
+          </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            Belt
+          </h1>
+          <span className="subtitle">
+            Shop now
+          </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            Pants
+          </h1>
+          <span className="subtitle">
+            Shop now
+          </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            Shoes
+          </h1>
+          <span className="subtitle">
+            Shop now
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
