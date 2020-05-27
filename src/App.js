af27de14ac66da4f61ea4e0f3da87ad560cfd4fc
AbstractRamen <jasperctools@gmail.com>
2020-05-27 23:06:08 +0900

    Header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage'
-import ShopPage from './pages/shop/shop'
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
